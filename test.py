0371e6290cb69fbb3153af9c45cc9d536c5a4101
Milton Österberg <miltonwendel@outlook.com>
2024-02-05 14:19:27 +0100

    first commmit

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("Hejsan")
+import matplotlib.pyplot as plt
+
+numbers = list(range(10))
+squares = [number**2 for number in numbers]
+
+print(numbers)
+print(squares)
+
+plt.plot(numbers, squares)
+plt.title("x*2 for positive integers 0-9")
+plt.show()
