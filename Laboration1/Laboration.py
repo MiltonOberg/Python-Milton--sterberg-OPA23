f29d25c23ec73814407b7ebc55b44e3ea488649f
Milton Österberg <miltonwendel@outlook.com>
2024-02-12 15:20:07 +0100

    classes for pikachu and pichu

---
+++ Laboration1/Laboration.py
@@ -0,0 +1,25 @@
+class Pikachu:
+    def __init__(self, width: float, height: float):
+        self._width = width
+        self._height = height
+    @property
+    def height(self):
+        return self._height
+    @property
+    def width(self):
+        return self._width
+
+class Pichu:
+    def __init__(self, width: float, height: float):
+        self._width = width
+        self._height = height
+    @property
+    def height(self):
+        return self._height
+    @property
+    def width(self):
+        return self._width
+
+class Program:
+    pikachu = Pikachu(26, 30)
+    print(pikachu.height, pikachu.width)
