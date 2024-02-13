c8f67c45511261a29db9fc941e1f982084ea3ba0
Milton Österberg <miltonwendel@outlook.com>
2024-02-13 16:38:28 +0100

    Transfering entity to the right class, also some notes

--- Laboration1/Laboration.py
+++ Laboration1/Laboration.py
@@ -23,5 +23,19 @@ class Pichu:
 
 
 class Userinterface:
-    
+    def userinterface():
+        import numpy as np
+        unidentified_width = float(input("Enter width of entity: "))
+        unidentified_height = float(input("Enter height of entity: "))
+
+
+        unidentified_distance_pikachu = np.sqrt((width_mean_pikachu - unidentified_width)**2 + (height_mean_pikachu - unidentified_height)**2) 
+        unidentified_distance_pichu = np.sqrt((width_mean_pichu - unidentified_width)**2 + (height_mean_pichu - unidentified_height)**2)
+
+        if unidentified_distance_pikachu < unidentified_distance_pichu:             #If Pikachu distance is smaler then Pichu's
+            print(f"You most likly found a Pikachu!")
+        elif unidentified_distance_pichu < unidentified_distance_pikachu:           #If Pichu distance is smaler then Pikachu's
+            print(f"You most likly found a Pichu!")
+        else:
+            print(F"I am unshore if you found a Pikachu or Pichu...")               #If the distance are the same
     
