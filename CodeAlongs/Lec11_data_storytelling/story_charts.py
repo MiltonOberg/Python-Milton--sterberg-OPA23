ece71c10df36a42b77efcc3e86aa8929d4956f7f
Milton Österberg <miltonwendel@outlook.com>
2024-03-07 11:26:51 +0100

    code along, #%%

--- CodeAlongs/Lec11_data_storytelling/story_charts.py
+++ CodeAlongs/Lec11_data_storytelling/story_charts.py
@@ -21,8 +21,8 @@ class StoryCharts:
         calling_method_name = currentframe().f_back.f_code.co_name
         if calling_method_name == "Line":
             self.ax.plot(x, y, color=colors)
-        elif calling_method_name == "bar":
-            self.ax.bar(x, y, colors=colors, width= 1)
+        elif calling_method_name == "Bar":
+            self.ax.bar(x, y, color=colors)
 
         self._set_labels(**label_kwargs)
         self.fig.tight_layout()
@@ -57,3 +57,4 @@ if __name__ == "__main__":
         ylabel="CO$_2$ MOLE FRACTION IN PPM",
         title="The annual mean of CO$_2$ emissions measured in Mauna Lao has\nincreased every year since 1959"
     )
+#%%
